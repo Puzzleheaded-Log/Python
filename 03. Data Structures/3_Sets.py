9c02f3f03af3c48d7b7af5cf36e1e09be3193f72
Adrian <adrianurdea96@gmail.com>
2026-05-26 13:17:16 +0300

    Update 3_Sets.py

--- 03. Data Structures/3_Sets.py
+++ 03. Data Structures/3_Sets.py
@@ -1,1 +1,31 @@
+""" Exercise 1: Remove duplicates from a list using a set. """
+
+numbers = [1, 2, 2, 3, 4, 4, 5]
+unique = set(numbers)
+print(unique)
+
+
+
+""" Exercise 2: Find first repeated event. """
+
+events = ["click", "scroll", "click", "hover"]
+
+seen = set()
+
+for e in events:
+    if e in seen:
+        print("Repeated event:", e)
+        break
+    seen.add(e)
+
+
+
+""" Exercise 3: Remove blocked users from active users. """
+
+active_users = {"ana", "ion", "maria", "alex"}
+blocked_users = {"alex", "ion"}
+
+allowed = active_users - blocked_users
+
+print(allowed)
 
