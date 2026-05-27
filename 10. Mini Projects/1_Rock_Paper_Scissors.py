7566072c0135a75490e8dda5087cc5c118dc0853
Adrian <adrianurdea96@gmail.com>
2026-05-27 09:03:36 +0300

    Update and rename 1_ex.py to 1_Rock_Paper_Scissors.py

---
+++ 10. Mini Projects/1_Rock_Paper_Scissors.py
@@ -0,0 +1,22 @@
+""" Create a Rock Paper Scissors game against the computer. """
+
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+computer = random.choice(choices)
+
+player = input("Choose rock, paper or scissors: ").lower()
+
+print("Computer chose:", computer)
+
+if player == computer:
+    print("Draw")
+elif (
+    (player == "rock" and computer == "scissors") or
+    (player == "paper" and computer == "rock") or
+    (player == "scissors" and computer == "paper")
+):
+    print("You win")
+else:
+    print("You lose")
