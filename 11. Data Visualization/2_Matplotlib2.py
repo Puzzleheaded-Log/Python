8c13b7cd5122d4387154403739ea5564258a3e61
Adrian <adrianurdea96@gmail.com>
2026-05-27 10:53:15 +0300

    Create 2_Matplotlib2.py

---
+++ 11. Data Visualization/2_Matplotlib2.py
@@ -0,0 +1,50 @@
+""" Create a line chart to visualize daily temperature data from a CSV file.
+Requirements:
+  - Load weather data from a CSV file using Python
+  - Extract dates, high temperatures, and low temperatures
+  - Convert date strings into datetime objects
+  - Store extracted data in separate lists
+  - Plot high and low temperatures using Matplotlib
+  - Fill the area between the two lines for better visualization
+  - Add chart title, axis labels, and improve readability of date labels
+  - Display the final plot.   """
+
+
+from pathlib import Path
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+path = Path("weather_data/sitka_weather_2021_simple.csv")
+lines = path.read_text(encoding="utf-8").splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+# Extract dates, high and low temperatures.
+dates, highs, lows = [], [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], "%Y-%m-%d")
+    high = int(row[4])
+    low = int(row[5])
+    dates.append(current_date)
+    highs.append(high)
+    lows.append(low)
+
+# Plot the high and low temperatures.
+plt.style.use("seaborn-v0_8")
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color="red", alpha=0.5)
+ax.plot(dates, lows, color="blue", alpha=0.5)
+ax.fill_between(dates, highs, lows, color="blue", alpha=0.1)
+
+# Format plot.
+ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
+ax.set_xlabel("", fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
+
